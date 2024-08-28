d0d2db6bd6297f91e89e0af9485ba75da881f637
watermelon <heng68807@gmail.com>
2024-08-28 16:41:48 +0700

    demo

---
+++ src/Demo.py
@@ -0,0 +1,20 @@
+import os
+import openai
+import tiktoken
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.environ['OPENAI_API_KEY']
+client = openai.OpenAI()
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+    return response.choices[0].message.content
+response = get_completion("Take the letters in lollipop \
+and reverse them")
+print(response)
