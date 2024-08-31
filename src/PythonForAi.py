6df14a70802ab35a2e853daf423456adaa58949d
watermelon <heng68807@gmail.com>
2024-08-31 23:22:29 +0700

    demo

--- src/PythonForAi.py
+++ src/PythonForAi.py
@@ -4,5 +4,6 @@ years ago would be in the same stage of life as Otto.""")
 dog_age = 12
 name = "watermelon"
 print(f"""{name}'s dog age is {dog_age}. So a dog that's about
+
 {dog_age} would be the same age as {name}. Any dog born about {dog_age}
 years ago would be in the same stage of life as {name}.""")
